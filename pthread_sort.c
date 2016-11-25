23216db5816c2ab057bf8e8812bdf66e8b74ce27
Nari Kan <>
2016-11-25 08:12:19 +0800

    try thread merge sort, but still fail

--- pthread_sort.c
+++ pthread_sort.c
@@ -39,7 +39,7 @@ int main(int argc, char *argv[])
         printf("%d ",my_array[i]);
     printf("\n");
 */
-   
+
 
     mergesort(my_array,backup,data_size);
 
@@ -65,9 +65,11 @@ void swap(int *a, int *b)
 
 void mergesort(int *source, int *temp, int number)
 {
-    int width,i;
+    int width,i,thread_count=0,join_idx;
     merge_struct test;
+    pthread_t thread_id[number];
     for (width=1; width<number; width*=2) {
+        thread_count=0;
         for (i=0; i<number; i+=2*width) {
             //merge_struct test;
             test.source = source;
@@ -75,19 +77,40 @@ void mergesort(int *source, int *temp, int number)
             test.left = i;
             test.mid = MIN(i+width,number);
             test.right = MIN(i+2*width,number);
-            real_merge_thread(&test);
+            if(pthread_create(&thread_id[thread_count++],NULL,&real_merge_thread,&test)) {
+                perror("Error in thread creation");
+                exit(-1);
+            }
+            //real_merge_thread(&test);
             //real_merge(source,temp,i,MIN(i+width,number),MIN(i+2*width,number));
         }
+        pthread_exit(NULL);
+        /*for (join_idx=0; join_idx<thread_count; join_idx++) {
+
+            if (pthread_equal(pthread_self(),thread_id[0])) {
+                continue;
+            }
+            else {
+                pthread_join(thread_id[0],NULL);
+            }
+
+            if (pthread_join(thread_id[join_idx],NULL)) {
+                perror("Pthread join error");
+                exit(-1);
+            }
+            printf("Index = %d is OK.\n",join_idx);
+        }*/
+
         copy_arr(temp,source,number);
     }
 
-    
+
 }
 
 void copy_arr(int *source, int *dest, int number)
 {
     int i;
-    for (i=0;i<number;i++) 
+    for (i=0;i<number;i++)
         dest[i] = source[i];
 }
 
@@ -111,11 +134,15 @@ void real_merge(int *source, int *temp, int left, int mid, int right)
 
 void* real_merge_thread(void *argument)
 {
+    printf("New thread: %lu\n",pthread_self());
+    pthread_mutex_t mylock;
+    pthread_mutex_lock(&mylock);
     merge_struct *a = (merge_struct*)argument;
     int i,j,k;
 
     i=a->left, j=a->mid;
     for (k=a->left; k<a->right; k++) {
+        //printf("i=%d k=%d\n",i,k);
         if (i<a->mid && (j>=a->right || a->source[i]<=a->source[j])) {
             a->temp[k] = a->source[i];
             i++;
@@ -125,6 +152,7 @@ void* real_merge_thread(void *argument)
             j++;
         }
     }
-
+    pthread_mutex_unlock(&mylock);
+    pthread_exit(NULL);
     return NULL;
 }
